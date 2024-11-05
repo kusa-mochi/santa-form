318941e578a9bc58adef2bc177be7583965a0606
kusa-mochi <whoatemyapplepie@gmail.com>
2024-11-05 18:04:03 +0900

    add 3rd page.

--- santa-form/renderer/pages/fin.tsx
+++ santa-form/renderer/pages/fin.tsx
@@ -7,7 +7,7 @@ export default function NextPage() {
   return (
     <React.Fragment>
       <Head>
-        <title>Next - Nextron (with-tailwindcss)</title>
+        <title>Fin - Nextron (with-tailwindcss)</title>
       </Head>
       <div className="grid grid-col-1 text-2xl w-full text-center">
         <div>
@@ -22,7 +22,7 @@ export default function NextPage() {
         <span>⚡ Nextron ⚡</span>
       </div>
       <div className="mt-1 w-full flex-wrap flex justify-center">
-        <Link href="/fin">Go to fin page</Link>
+        <Link href="/home">Go to home page</Link>
       </div>
     </React.Fragment>
   )
