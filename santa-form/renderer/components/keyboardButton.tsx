6fc772424dc6066ee17511a8e70d4705065ac37a
kusa-mochi <whoatemyapplepie@gmail.com>
2024-11-09 20:37:59 +0900

    make hover & active styles of the keyboard button.

--- santa-form/renderer/components/keyboardButton.tsx
+++ santa-form/renderer/components/keyboardButton.tsx
@@ -9,9 +9,9 @@ export default function KeyboardButton(props: Props) {
     return (
         <>
             {props.label !== null &&
-            <div onClick={() => props.onClick(props.label)} className="w-12 h-12 bg-gray-500 flex justify-center items-center text-3xl">
+            <button onClick={() => props.onClick(props.label)} className="w-12 h-12 bg-gray-500 flex justify-center items-center text-3xl select-none hover:bg-gray-600 active:bg-gray-700">
                 {props.label}
-            </div>}
+            </button>}
             {props.label === null &&
             <div className="w-12 h-12">
                 {/* no content */}
