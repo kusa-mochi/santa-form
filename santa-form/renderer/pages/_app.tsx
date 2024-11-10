21f416dd492e5fec53da532f701af976454c4b73
kusa-mochi <whoatemyapplepie@gmail.com>
2024-11-10 16:12:53 +0900

    apply context provider.

--- santa-form/renderer/pages/_app.tsx
+++ santa-form/renderer/pages/_app.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.css'
+import MailContext from '../contexts/mailContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [mailText, setMailText] = useState("")
+  return (
+    <MailContext.Provider value={{mailText, setMailText}}>
+      <Component {...pageProps} />
+    </MailContext.Provider>
+  )
 }
 
 export default MyApp
