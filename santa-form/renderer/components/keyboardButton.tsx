9854afcacb0b8850f22d50835723d31c2b2ca4a0
kusa-mochi <whoatemyapplepie@gmail.com>
2024-11-12 20:28:15 +0900

    fix keyboard button colors.

--- santa-form/renderer/components/keyboardButton.tsx
+++ santa-form/renderer/components/keyboardButton.tsx
@@ -9,7 +9,7 @@ export default function KeyboardButton(props: Props) {
     return (
         <>
             {props.label !== null &&
-            <button onClick={() => props.onClick(props.label)} className="w-12 h-12 bg-gray-500 flex justify-center items-center text-3xl select-none hover:bg-gray-600 active:bg-gray-700">
+            <button onClick={() => props.onClick(props.label)} className="w-12 h-12 bg-gray-300 flex justify-center items-center text-3xl select-none hover:bg-gray-400 active:bg-gray-500">
                 {props.label}
             </button>}
             {props.label === null &&
